2d5968abb90043d99fc227a18266ad44453d6313
Сергей <7atom7@mail.ru>
2020-12-20 13:04:34 +0300

    Third

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
 import datetime as dt
 
 
-# For records
 class Record:
+    '''
+    The class required for the records.
+    '''
     def __init__(self, amount, comment, date=None):
-        # sum money|calories
         self.amount = amount
-        # comments
         self.comment = comment
         self.date = date
         if self.date is None:
@@ -15,8 +15,12 @@ class Record:
             self.date = dt.datetime.strptime(self.date, '%d.%m.%Y').date()
 
 
-# Main Calc
 class Calculator:
+    '''
+    The class to which new entries are added.
+    Which accepts a calorie/money limit.
+    And calculates the amount spent during this period.
+    '''
     def __init__(self, limit):
         self.limit = limit
         self.records = []
@@ -24,22 +28,24 @@ class Calculator:
     def add_record(self, record):
         self.records.append(record)
 
-    # Total cash spent today:
     def get_today_stats(self):
-        return sum(r_i.amount
-                   for r_i in self.records
-                   if (r_i.date == dt.date.today()))
+        return sum(record_i.amount
+                   for record_i in self.records
+                   if (record_i.date == dt.date.today()))
 
-    # Total cash spent week:
     def get_week_stats(self):
-        return sum(r_num.amount
-                   for r_num in self.records
-                   if (r_num.date >= dt.date.today() - dt.timedelta(days=7)
-                       and (r_num.date <= dt.date.today())))
+        return sum(record_n.amount
+                   for record_n in self.records
+                   if (record_n.date >= dt.date.today() - dt.timedelta(days=7)
+                       and (record_n.date <= dt.date.today())))
 
 
-# Money calc:
 class CashCalculator(Calculator):
+    '''
+    Money calculator.
+    Calculates the money spent
+    and compares it with the limit for today.
+    '''
     USD_RATE = 60.0
     EURO_RATE = 70.0
     dictionary_rates = {
@@ -49,19 +55,25 @@ class CashCalculator(Calculator):
         }
 
     def get_today_cash_remained(self, currency):
+        cash_rate, cash_name = self.dictionary_rates[currency]
         cash_today = self.limit - self.get_today_stats()
+        remain = round((cash_today)/cash_rate, 2)
         if cash_today > 0:
-            return f'На сегодня осталось {round((cash_today)/self.dictionary_rates[currency][0], 2)} {self.dictionary_rates[currency][1]}'
+            return f'На сегодня осталось {remain} {cash_name}'
         elif cash_today < 0:
-            return f'Денег нет, держись: твой долг - {abs(round((cash_today)/self.dictionary_rates[currency][0], 2))} {self.dictionary_rates[currency][1]}'
+            return f'Денег нет, держись: твой долг - {abs(remain)} {cash_name}'
         return 'Денег нет, держись'
 
 
-# Calories calc:
 class CaloriesCalculator(Calculator):
-    # Calories were eaten today:
+    '''
+    Calorie calculator.
+    Calculates the calories consumed
+    and compares it with the limit for today.
+    '''
     def get_calories_remained(self):
         if self.limit > self.get_today_stats() > 0:
             calories_remained = self.limit - self.get_today_stats()
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories_remained} кКал'
+            return ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                    f'калорийностью не более {calories_remained} кКал')
         return 'Хватит есть!'
